cbcfcd1d04c82f0b02c90f61de084f359c4547b2
Zhalgasbek <zhalgasbek1599@gmail.com>
2023-08-28 09:59:50 +0600

    new commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<bluetooth_print/BluetoothPrintPlugin.h>)
+#import <bluetooth_print/BluetoothPrintPlugin.h>
+#else
+@import bluetooth_print;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [BluetoothPrintPlugin registerWithRegistrar:[registry registrarForPlugin:@"BluetoothPrintPlugin"]];
+}
+
+@end
